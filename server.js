8b1928a4eb876e937962d1ba2cc475455c10fc2e
Mélanie <belard.melanie@gmail.com>
2018-03-19 21:17:08 +0100

    update

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var redis = require("redis"),
 
 var server = http.createServer(function(req, res) {
 
+	// res.write("tamer");
+	// res.end();
+
     var page = url.parse(req.url).pathname;
+    var id = "";
     var notes = [];
     var response = "";
     console.log(page);
@@ -19,19 +23,37 @@ var server = http.createServer(function(req, res) {
 
 		console.log("On est sur les notes.");
 		client.keys("*note*", function(err, replies) {
-			replies.forEach(function(reply, i) {
-				response += "    " + i + ": " + reply;
-				//console.log("    " + i + ": " + reply);
+			replies.forEach(function(key, i) {
+				// client.get(key, redis.print);
+				// response += client.get(key);
+				client.get(key, function(err, reply) {
+					console.log("    " + key + ": " + reply);
+					response += "    " + key + ": " + reply;
+					res.write(reply);
+				});
+					res.write(response);
+				// console.log(response);
 			});
-			console.log(response);
-			res.writeHead(404, {"Content-Type": "text/html"});
-			res.end(response);
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end();
 			//client.quit();
 		});
+	} else if (page.includes("/notes/")) {
+		console.log("On a un ID !");
+		id = page.substring(("/notes/").length);
+		client.get("note"+id, function(err, reply) {
+			console.log(reply);
+			res.write(reply);
+			res.end();
+		});
+		console.log(id);
 	} else {
 	    res.writeHead(404, {"Content-Type": "text/html"});
-	    res.end("No response.");
+	    response = "No response";
+	    res.write(response);
+	    res.end();
 	}
+
 
 	/*client.hkeys("hash key", function (err, replies) {
 	    console.log(replies.length + " replies:");
@@ -40,6 +62,8 @@ var server = http.createServer(function(req, res) {
 	    });
 	    client.quit();
 	});*/
+
+	// res.end();
 
 });
 
